53d489db37b0191041fd4d9d428474d974e21ba7
Lut99 <mullertim99@gmail.com>
2020-07-04 18:17:16 +0200

    (#8, #13) Added the missing functions to the Vec3-class. Additionally, started on merging GVec3 and Vec3 into a single class.

--- src/lib/math/Vec3.cpp
+++ src/lib/math/Vec3.cpp
@@ -4,7 +4,7 @@
  * Created:
  *   6/30/2020, 5:09:06 PM
  * Last edited:
- *   7/1/2020, 4:15:30 PM
+ *   7/4/2020, 6:06:18 PM
  * Auto updated?
  *   Yes
  *
@@ -21,28 +21,71 @@ using namespace std;
 using namespace RayTracer;
 
 
-Vec3::Vec3() :
-    x(0),
-    y(0),
-    z(0)
-{}
+HOST_DEVICE Vec3::Vec3() :
+    x(local_x),
+    y(local_y),
+    z(local_z)
+{
+    this->is_external = false;
+    this->local_x = 0;
+    this->local_y = 0;
+    this->local_z = 0;
+}
 
-Vec3::Vec3(double x, double y, double z) :
-    x(x),
-    y(y),
-    z(z)
-{}
+HOST_DEVICE Vec3::Vec3(double x, double y, double z) :
+    x(local_x),
+    y(local_y),
+    z(local_z)
+{
+    this->is_external = false;
+    this->local_x = x;
+    this->local_y = y;
+    this->local_z = z;
+}
+
+#ifdef CUDA
+__device__ Vec3::Vec3(void* data) :
+    external_data((double*) data),
+    x(this->external_data[0]),
+    y(this->external_data[1]),
+    z(this->external_data[2])
+{
+    this->is_external = true;
+}
+#endif
+
+HOST_DEVICE Vec3::Vec3(const Vec3& other) :
+    x(local_x),
+    y(local_y),
+    z(local_z)
+{
+    this->is_external = false;
+    this->local_x = other.x;
+    this->local_y = other.y;
+    this->local_z = other.z;
+}
+
+HOST_DEVICE Vec3::Vec3(Vec3&& other) :
+    x(local_x),
+    y(local_y),
+    z(local_z)
+{
+    this->is_external = false;
+    this->local_x = other.x;
+    this->local_y = other.y;
+    this->local_z = other.z;
+}
 
 
 
-Vec3& Vec3::operator+=(const double c) {
+HOST_DEVICE Vec3& Vec3::operator+=(const double c) {
     this->x += c;
     this->y += c;
     this->z += c;
     return *this;
 }
 
-Vec3& Vec3::operator+=(const Vec3& other) {
+HOST_DEVICE Vec3& Vec3::operator+=(const Vec3& other) {
     this->x += other.x;
     this->y += other.y;
     this->z += other.z;
@@ -51,14 +94,14 @@ Vec3& Vec3::operator+=(const Vec3& other) {
 
 
 
-Vec3& Vec3::operator-=(const double c) {
+HOST_DEVICE Vec3& Vec3::operator-=(const double c) {
     this->x -= c;
     this->y -= c;
     this->z -= c;
     return *this;
 }
 
-Vec3& Vec3::operator-=(const Vec3& other) {
+HOST_DEVICE Vec3& Vec3::operator-=(const Vec3& other) {
     this->x -= other.x;
     this->y -= other.y;
     this->z -= other.z;
@@ -67,14 +110,14 @@ Vec3& Vec3::operator-=(const Vec3& other) {
 
 
 
-Vec3& Vec3::operator*=(const double c) {
+HOST_DEVICE Vec3& Vec3::operator*=(const double c) {
     this->x *= c;
     this->y *= c;
     this->z *= c;
     return *this;
 }
 
-Vec3& Vec3::operator*=(const Vec3& other) {
+HOST_DEVICE Vec3& Vec3::operator*=(const Vec3& other) {
     this->x *= other.x;
     this->y *= other.y;
     this->z *= other.z;
@@ -83,41 +126,55 @@ Vec3& Vec3::operator*=(const Vec3& other) {
 
 
 
-double Vec3::operator[](const size_t i) const {
+HOST_DEVICE Vec3& Vec3::operator=(Vec3 other) {
+    // Never ourselves, just swap lmao
+    swap(*this, other);
+    return *this;
+}
+
+HOST_DEVICE Vec3& Vec3::operator=(Vec3&& other) {
+    // Only swap if ourselvse
+    if (this == &other) {
+        swap(*this, other);
+    }
+    return *this;
+}
+
+HOST_DEVICE void RayTracer::swap(Vec3& v1, Vec3& v2) {
+    // Simply swap x, y and z; the referenced items will follow, and we needn't change whether it's external or not.
+    double t = v1.x;
+    v1.x = v2.x;
+    v2.x = t;
+
+    t = v1.y;
+    v1.y = v2.y;
+    v2.y = t;
+
+    t = v1.z;
+    v1.z = v2.z;
+    v2.z = t;
+}
+
+
+
+HOST_DEVICE double Vec3::operator[](const size_t i) const {
     if (i == 0) { return this->x; }
     else if (i == 1) { return this->y; }
     else if (i == 2) { return this->z; }
     
     // Else...
-    throw out_of_range("ERROR: double Vec3::operator[](const size_t i) const: Index " + to_string(i) + " is out of bounds for vector of length 3.");
+    printf("ERROR: double Vec3::operator[](const size_t i) const: Index %lu is out of bounds for vector of length 3.", i);
+    return -INFINITY;
 }
 
-double& Vec3::operator[](const size_t i) {
+HOST_DEVICE double& Vec3::operator[](const size_t i) {
     if (i == 0) { return this->x; }
     else if (i == 1) { return this->y; }
     else if (i == 2) { return this->z; }
     
     // Else...
-    throw out_of_range("ERROR: double& Vec3::operator[](const size_t i): Index " + to_string(i) + " is out of bounds for vector of length 3.");
-}
-
-
-
-/* Performs the exp-operation on each element of the vector. */
-Vec3 RayTracer::exp(const Vec3& vec) {
-    return Vec3(exp(vec.x), exp(vec.y), exp(vec.z));
-}
-/* Performs the sqrt-operation on each element of the vector. */
-Vec3 RayTracer::sqrt(const Vec3& vec) {
-    return Vec3(sqrt(vec.x), sqrt(vec.y), sqrt(vec.z));
-}
-/* Performs the pow-operation on each element of the vector. */
-Vec3 RayTracer::pow(const Vec3& vec, const double c) {
-    return Vec3(pow(vec.x, c), pow(vec.y, c), pow(vec.z, c));
-}
-/* Performs the fabs-operation on each element of the vector. */
-Vec3 RayTracer::fabs(const Vec3& vec) {
-    return Vec3(fabs(vec.x), fabs(vec.y), fabs(vec.z));
+    printf("ERROR: double& Vec3::operator[](const size_t i): Index %lu is out of bounds for vector of length 3.", i);
+    return this->x;
 }
 
 
--- src/lib/math/Vec3.hpp
+++ src/lib/math/Vec3.hpp
@@ -4,7 +4,7 @@
  * Created:
  *   6/30/2020, 5:08:07 PM
  * Last edited:
- *   7/1/2020, 4:35:47 PM
+ *   7/4/2020, 5:59:07 PM
  * Auto updated?
  *   Yes
  *
@@ -23,6 +23,12 @@
 #include <ostream>
 #include <cmath>
 
+#ifdef CUDA
+#define HOST_DEVICE __host__ __device__
+#else
+#define HOST_DEVICE
+#endif
+
 namespace RayTracer {
     using ::exp;
     using ::sqrt;
@@ -30,105 +36,142 @@ namespace RayTracer {
     using ::fabs;
 
     class Vec3 {
+    private:
+        /* Determines if the vector is using external memory or stores it in the object memory. */
+        bool is_external;
+        /* Local value storage. */
+        double local_x, local_y, local_z;
+        /* External storage reference. */
+        double* external_data;
+
     public:
-        /* The coordinates of the vector. */
-        double x, y, z;
+        /* The x-coordinate of the vector. */
+        double& x;
+        /* The y-coordinate of the vector. */
+        double& y;
+        /* The z-coordinate of the vector. */
+        double& z;
 
         /* Default constructor for the Vec3-class which initializes a vector with all-zero elements. */
-        Vec3();
+        HOST_DEVICE Vec3();
         /* Constructor which takes three elements for the vector to be initialized with. */
-        Vec3(double x, double y, double z);
+        HOST_DEVICE Vec3(double x, double y, double z);
+        #ifdef CUDA
+        /* GPU-side constructor for using external memory. */
+        __device__ Vec3(void* data);
+        #endif
+        /* Copy constructor for the Vec3-class. */
+        HOST_DEVICE Vec3(const Vec3& other);
+        /* Move constructor for the Vec3-class. */
+        HOST_DEVICE Vec3(Vec3&& other);
 
         /* Compares if two vectors are equal. */
-        inline bool operator==(const Vec3& other) const { return this->x == other.x && this->y == other.y && this->z == other.z; }
+        HOST_DEVICE inline bool operator==(const Vec3& other) const { return this->x == other.x && this->y == other.y && this->z == other.z; }
         /* Compares if two vectors are not equal. */
-        inline bool operator!=(const Vec3& other) const { return this->x != other.x || this->y != other.y || this->z != other.z; }
+        HOST_DEVICE inline bool operator!=(const Vec3& other) const { return this->x != other.x || this->y != other.y || this->z != other.z; }
         /* Compares if all elements in a vector are less than a given constant. */
-        inline bool operator<(const double c) const { return this->x < c && this->y < c && this->z < c; }
+        HOST_DEVICE inline bool operator<(const double c) const { return this->x < c && this->y < c && this->z < c; }
         /* Compares if all elements in a vector are less than or equal to a given constant. */
-        inline bool operator<=(const double c) const { return this->x <= c && this->y <= c && this->z <= c; }
+        HOST_DEVICE inline bool operator<=(const double c) const { return this->x <= c && this->y <= c && this->z <= c; }
         /* Compares if all elements in a vector are greater than a given constant. */
-        inline bool operator>(const double c) const { return this->x > c && this->y > c && this->z > c; }
+        HOST_DEVICE inline bool operator>(const double c) const { return this->x > c && this->y > c && this->z > c; }
         /* Compares if all elements in a vector are greater than or equal to a given constant. */
-        inline bool operator>=(const double c) const { return this->x >= c && this->y >= c && this->z >= c; }
-        
+        HOST_DEVICE inline bool operator>=(const double c) const { return this->x >= c && this->y >= c && this->z >= c; }
 
         /* Adds a constant to all elements in the vector and returns the result as a new one. */
-        inline Vec3 operator+(const double c) const { return Vec3(this->x + c, this->y + c, this->z + c); }
+        HOST_DEVICE inline Vec3 operator+(const double c) const { return Vec3(this->x + c, this->y + c, this->z + c); }
         /* Adds a constant to all elements in the vector and returns the result in this one. */
-        Vec3& operator+=(const double c);
+        HOST_DEVICE Vec3& operator+=(const double c);
         /* Adds another Vec3-object to this vector (element-wise) and returns the result in a new one. */
-        inline Vec3 operator+(const Vec3& other) const { return Vec3(this->x + other.x, this->y + other.y, this->z + other.z); }
+        HOST_DEVICE inline Vec3 operator+(const Vec3& other) const { return Vec3(this->x + other.x, this->y + other.y, this->z + other.z); }
         /* Adds another Vec3-object to this vector (element-wise) and returns the result in this one. */
-        Vec3& operator+=(const Vec3& other);
+        HOST_DEVICE Vec3& operator+=(const Vec3& other);
 
         /* Return a copy of this vector with all elements negated. */
-        inline Vec3 operator-() const { return Vec3(-this->x, -this->y, -this->z); }
+        HOST_DEVICE inline Vec3 operator-() const { return Vec3(-this->x, -this->y, -this->z); }
         /* Subtracts a constant from all elements in the vector and returns the result as a new one. */
-        inline Vec3 operator-(const double c) const { return Vec3(this->x - c, this->y - c, this->z - c); }
+        HOST_DEVICE inline Vec3 operator-(const double c) const { return Vec3(this->x - c, this->y - c, this->z - c); }
         /* Subtracts a constant from all elements in the vector and returns the result in this one. */
-        Vec3& operator-=(const double c);
+        HOST_DEVICE Vec3& operator-=(const double c);
         /* Subtracts another Vec3-object from this vector (element-wise) and returns the result in a new one. */
-        inline Vec3 operator-(const Vec3& other) const { return Vec3(this->x - other.x, this->y - other.y, this->z - other.z); }
+        HOST_DEVICE inline Vec3 operator-(const Vec3& other) const { return Vec3(this->x - other.x, this->y - other.y, this->z - other.z); }
         /* Subtracts another Vec3-object from this vector (element-wise) and returns the result in this one. */
-        Vec3& operator-=(const Vec3& other);
+        HOST_DEVICE Vec3& operator-=(const Vec3& other);
 
         /* Multiplies a constant with all elements in the vector and returns the result as a new one. */
-        inline Vec3 operator*(const double c) const { return Vec3(this->x * c, this->y * c, this->z * c); }
+        HOST_DEVICE inline Vec3 operator*(const double c) const { return Vec3(this->x * c, this->y * c, this->z * c); }
         /* Multiplies a constant with all elements in the vector and returns the result in this one. */
-        Vec3& operator*=(const double c);
+        HOST_DEVICE Vec3& operator*=(const double c);
         /* Multiplies another Vec3-object with this vector (element-wise) and returns the result in a new one. */
-        inline Vec3 operator*(const Vec3& other) const { return Vec3(this->x * other.x, this->y * other.y, this->z * other.z); }
+        HOST_DEVICE inline Vec3 operator*(const Vec3& other) const { return Vec3(this->x * other.x, this->y * other.y, this->z * other.z); }
         /* Multiplies another Vec3-object with this vector (element-wise) and returns the result in this one. */
-        Vec3& operator*=(const Vec3& other);
+        HOST_DEVICE Vec3& operator*=(const Vec3& other);
 
         /* Inverts all elements in this vector (1/x) and returns a copy. */
-        inline Vec3 inv() const { return Vec3(1 / this->x, 1 / this->y, 1 / this->z); }
+        HOST_DEVICE inline Vec3 inv() const { return Vec3(1 / this->x, 1 / this->y, 1 / this->z); }
         /* Divides all elements in this vector by a constant and returns the result as a new one. */
-        inline Vec3 operator/(const double c) const { return Vec3(this->x / c, this->y / c, this->z / c); }
+        HOST_DEVICE inline Vec3 operator/(const double c) const { return Vec3(this->x / c, this->y / c, this->z / c); }
         /* Divides all elements in this vector by a constant and returns the result in this one. */
-        inline Vec3& operator/=(const double c) { return *this *= (1 / c); }
+        HOST_DEVICE inline Vec3& operator/=(const double c) { return *this *= (1 / c); }
         /* Divides all elements in this vector by another vector (element-wise) and returns the result in a new one. */
-        inline Vec3 operator/(const Vec3& other) const { return Vec3(this->x / other.x, this->y / other.y, this->z / other.z); }
+        HOST_DEVICE inline Vec3 operator/(const Vec3& other) const { return Vec3(this->x / other.x, this->y / other.y, this->z / other.z); }
         /* Divides all elements in this vector by another vector (element-wise) and returns the result in this one. */
-        inline Vec3& operator/=(const Vec3& other) { return *this *= other.inv(); }
+        HOST_DEVICE inline Vec3& operator/=(const Vec3& other) { return *this *= other.inv(); }
 
         /* Returns the sum of all elements of this vector. */
-        inline double sum() const { return this->x + this->y + this->z; }
+        HOST_DEVICE inline double sum() const { return this->x + this->y + this->z; }
         /* Returns the length of this vector. */
-        inline double length() const { return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); }
+        HOST_DEVICE inline double length() const { return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); }
         /* Returns the squared length of this vector. */
-        inline double length_pow2() const { return this->x * this->x + this->y * this->y + this->z * this->z; }
+        HOST_DEVICE inline double length_pow2() const { return this->x * this->x + this->y * this->y + this->z * this->z; }
+        /* Returns the normalized version of this vector. */
+        HOST_DEVICE inline Vec3 normalize() const { return Vec3(*this) / this->length(); }
+
+        /* Copy assignment operator for the Vec3-class. */
+        HOST_DEVICE Vec3& operator=(Vec3 other);
+        /* Move assignment operator for the Vec3-class. */
+        HOST_DEVICE Vec3& operator=(Vec3&& other);
+        /* Swap operator for the Vec3-class. */
+        friend HOST_DEVICE void swap(Vec3& v1, Vec3& v2);
 
         /* Returns x, y or z based on their index (non-mutable). */
-        double operator[](const size_t i) const;
+        HOST_DEVICE double operator[](const size_t i) const;
         /* Returns x, y or z based on their index (mutable). */
-        double& operator[](const size_t i);
+        HOST_DEVICE double& operator[](const size_t i);
 
-        /* Allows exp to work on the vector. */
-        friend Vec3 exp(const Vec3& vec);
-        /* Allows sqrt to work on the vector. */
-        friend Vec3 sqrt(const Vec3& vec);
-        /* Allows pow to work on the vector. */
-        friend Vec3 pow(const Vec3& vec);
-        /* Allows fabs to work on the vector. */
-        friend Vec3 fabs(const Vec3& vec);
+        #ifdef CUDA
+        /* Copies the Vec3 object to the GPU. */
+        void* toGPU() const;
+        /* Copies the Vec3 object from the GPU to a new CPU-side object. */
+        static Vec3 fromGPU(void* ptr_gpu);
+        #endif
 
         /* Allows the vector to be printed to a stream. */
         friend std::ostream& operator<<(std::ostream& os, const Vec3& vec);
     };
 
-    /* Performs the exp-operation on each element of the vector. */
-    Vec3 exp(const Vec3& vec);
-    /* Performs the sqrt-operation on each element of the vector. */
-    Vec3 sqrt(const Vec3& vec);
-    /* Performs the pow-operation on each element of the vector. */
-    Vec3 pow(const Vec3& vec, const double c);
-    /* Performs the fabs-operation on each element of the vector. */
-    Vec3 fabs(const Vec3& vec);
+    /* Swaps two Vec3-objects. */
+    HOST_DEVICE void swap(Vec3& v1, Vec3& v2);
 
-    /* Allows the vector to be printed to a stream. */
+    /* Allows the vector to be printed to a stream (CPU-only). */
     std::ostream& operator<<(std::ostream& os, const Vec3& vec);
+
+    /* Performs the sum-operation to sum all elements of a vector. */
+    HOST_DEVICE inline double sum(const Vec3& vec) { return vec.x + vec.y + vec.z; }
+    /* Performs the exp-operation on each element of the vector. */
+    HOST_DEVICE inline Vec3 exp(const Vec3& vec) { return Vec3(exp(vec.x), exp(vec.y), exp(vec.z)); }
+    /* Performs the sqrt-operation on each element of the vector. */
+    HOST_DEVICE inline Vec3 sqrt(const Vec3& vec) { return Vec3(sqrt(vec.x), sqrt(vec.y), sqrt(vec.z)); }
+    /* Performs the pow-operation on each element of the vector. */
+    HOST_DEVICE inline Vec3 pow(const Vec3& vec, const double c) { return Vec3(pow(vec.x, c), pow(vec.y, c), pow(vec.z, c)); }
+    /* Performs the fabs-operation on each element of the vector. */
+    HOST_DEVICE inline Vec3 fabs(const Vec3& vec) { return Vec3(fabs(vec.x), fabs(vec.y), fabs(vec.z)); }
+    /* Performs a dot product between two vectors. */
+    HOST_DEVICE inline double dot(const Vec3& v1, const Vec3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+    /* Performs the cross product between two vectors. */
+    HOST_DEVICE inline Vec3 cross(const Vec3& v1, const Vec3& v2) { return Vec3(v1.y * v2.z - v1.z * v2.y,
+                                                                                v1.z * v2.x - v1.x * v2.z,
+                                                                                v1.x * v2.y - v1.y * v2.x); }
 }
 
 #endif
